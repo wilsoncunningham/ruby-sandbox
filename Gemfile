cc9e906d5f988acc3843a237a22728249d647236
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-21 23:45:16 +0000

    45 minutes into class

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 # this file, always named "Gemfile" (no suffix), informs other users of dependencies for running these files
-
+# to install these gem dependencies in your computer, run the "bundle install" command in the terminal
 
 source "https://rubygems.org"
 
--- hello.rb
+++ hello.rb
@@ -1,16 +1,26 @@
-pp "Hello, world!"
+# pp "Hello, world!"
 
-require "./goodbye.rb"
-# the ./ is the relative path to that file
+# require "./goodbye.rb"
+# # the ./ is the relative path to that file
 
-require "active_support/all"
+# require "active_support/all"
 
-pp 12.ordinalize # activesupport method
+# pp 12.ordinalize # activesupport method
 
-x = "    Wilson     Cunningham   "
-pp x.squish       # activesupport method
+# x = "    Wilson     Cunningham   "
+# pp x.squish       # activesupport method
 
 ### other useful gems:
   # require "pry-byebug"
   # require "better_errors"
   # require "ruby-openai"
+  # gem "awesome_print"
+
+
+require "active_support/all"
+require "pry-byebug"
+require "awesome_print"
+
+x = gets.chomp
+
+pp x
