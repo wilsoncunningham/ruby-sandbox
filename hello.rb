9577434a22b00ae374b9aa2f98cb7062a248bc40
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-21 23:27:05 +0000

    first 25 minutes of class

---
+++ hello.rb
@@ -0,0 +1,16 @@
+pp "Hello, world!"
+
+require "./goodbye.rb"
+# the ./ is the relative path to that file
+
+require "active_support/all"
+
+pp 12.ordinalize # activesupport method
+
+x = "    Wilson     Cunningham   "
+pp x.squish       # activesupport method
+
+### other useful gems:
+  # require "pry-byebug"
+  # require "better_errors"
+  # require "ruby-openai"
